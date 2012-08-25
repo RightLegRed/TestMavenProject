4a81f77d1f18442c17cee2233e756ffe8aec4789
Nathan Dawson <dragommer@gmail.com>
2012-08-25 12:55:25 +0100

    Test

---
+++ src/main/java/com/fedelaus/tstmvn/MainTest.java
@@ -0,0 +1,15 @@
+package com.fedelaus.tstmvn;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Archelaus
+ * Date: 25/08/12
+ * Time: 11:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainTest {
+
+    static public void main(String[] args){
+        System.out.println("hello");
+    }
+}
